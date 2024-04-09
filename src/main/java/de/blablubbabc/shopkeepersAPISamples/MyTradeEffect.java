76961c85a7fabf152ddfce8fea779cd15b0079ab
blablubbabc <lukas@wirsindwir.de>
2024-04-09 18:33:01 +0200

    Add more examples: Item editing and trade effects

---
+++ src/main/java/de/blablubbabc/shopkeepersAPISamples/MyTradeEffect.java
@@ -0,0 +1,19 @@
+package de.blablubbabc.shopkeepersAPISamples;
+
+import com.nisovin.shopkeepers.api.events.ShopkeeperTradeEvent;
+import com.nisovin.shopkeepers.api.trading.TradeEffect;
+
+public class MyTradeEffect implements TradeEffect {
+
+	@Override
+	public void onTradeAborted(ShopkeeperTradeEvent shopkeeperTradeEvent) {
+		// Trade aborted.
+	}
+
+	@Override
+	public void onTradeApplied(ShopkeeperTradeEvent shopkeeperTradeEvent) {
+		// Apply additional effects.
+		// Note: Don't attempt to edit the event at this stage, since we are past all the default
+		// handling.
+	}
+}
--- src/main/java/de/blablubbabc/shopkeepersAPISamples/ShopkeepersListener.java
+++ src/main/java/de/blablubbabc/shopkeepersAPISamples/ShopkeepersListener.java
@@ -3,21 +3,34 @@ package de.blablubbabc.shopkeepersAPISamples;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.nisovin.shopkeepers.api.events.ShopkeeperOpenUIEvent;
 import com.nisovin.shopkeepers.api.events.ShopkeeperTradeEvent;
+import com.nisovin.shopkeepers.api.util.UnmodifiableItemStack;
 
 public class ShopkeepersListener implements Listener {
 
 	@EventHandler
-	public void onShopKeeperTrade(ShopkeeperTradeEvent e) {
-		Player player = e.getPlayer();
-		player.sendMessage("Trade");
+	public void onShopKeeperTrade(ShopkeeperTradeEvent event) {
+		Player player = event.getPlayer();
+		player.sendMessage("Trading: Replacing the result item.");
+
+		ItemStack newResultItem = event.getResultItem().copy();
+		ItemMeta itemMeta = newResultItem.getItemMeta();
+		itemMeta.setUnbreakable(true);
+		newResultItem.setItemMeta(itemMeta);
+
+		event.setResultItem(UnmodifiableItemStack.of(newResultItem));
+
+		// We can also add trade effects that are invoked once the trade is applied:
+		event.getTradeEffects().add(new MyTradeEffect());
 	}
 
 	@EventHandler
-	public void onShopKeeperTrade(ShopkeeperOpenUIEvent e) {
-		Player player = e.getPlayer();
-		player.sendMessage("Open UI");
+	public void onShopKeeperTrade(ShopkeeperOpenUIEvent event) {
+		Player player = event.getPlayer();
+		player.sendMessage("Opening trading UI ...");
 	}
 }
